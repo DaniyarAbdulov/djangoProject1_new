d92b112f7ba6739ce428dae769922a496a620f44
daniyarabdulov <daniyar0303@gmail.com>
2023-01-30 22:17:36 +0600

    new Project

---
+++ products/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from . import models
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True)
+
+    class Meta:
+        model = models.ProductImage
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True)
+
+    class Meta:
+        model = models.Product
+        fields = '__all__'
+
+
+class RetrieveProductSerializer(ProductSerializer):
+    product_images = ProductImageSerializer(many=True, read_only=True)
+
+
+
+
